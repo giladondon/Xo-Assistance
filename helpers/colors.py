97370048ed935684aa091046e164090a35d46ee4
Gilad Barak <giladondon@gmail.com>
2025-08-09 16:51:54 +0300

    Now working, label system is well
    still need to update excel by the team

--- helpers/colors.py
+++ helpers/colors.py
@@ -1,8 +1,13 @@
 LABEL_TO_COLOR = {
-    "פיקוד": "11",      # red
-    "הנדסה": "7",       # green
-    "כלל צוות": "2",    # lavender
-    # add your own mappings here…
+    "טכנית": "6",      # orange
+    "מבצעים": "11",       # Red
+    "גנק": "3",    # Purple
+    "פיקוד": "8",  # Graphite
+    "סונר": "9",  # blueberry
+    "נשק": "10",  # basil green
+    "מפקד": "7",  # cyan
+    "סגן": "2",  # peacock
+    "צוות": "1",  # lavender
 }
 
 def color_for_label(label):
